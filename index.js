9910728e8fa605580f13a3669188bfa5e2de570e
AloneMadara <41944650+AloneMadara@users.noreply.github.com>
2018-07-31 11:47:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let Discord = require('discord.js');
 let client = new Discord.Client();
 function play () {
-	let dispatcher = conn.playFile(`./${process.env.NUM}.mp3`);
-	dispatcher.on('end', play);
+    let dispatcher = conn.playFile(`./${process.env.NUM}.mp3`);
+    dispatcher.on('end', play);
 }
 let arr = [
 '','',''
 ]
-client.channels.get(arr[parseInt(process.env.NUM)]).join().then(conn => {
-	play();
-})
+client.on('ready', () => {
+
+    console.log('authorised as ' + client.user.tag );
+    client.channels.get(arr[parseInt(process.env.NUM)-1]).join().then(conn => {
+        play();
+    })
+});
 client.login(process.env.TOKEN)
