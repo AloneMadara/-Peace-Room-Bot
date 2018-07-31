5f430feda3a4517ee2e9dd47c281bd2226f96f60
AloneMadara <41944650+AloneMadara@users.noreply.github.com>
2018-07-31 11:38:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let Discord = require('discord.js');
+let client = new Discord.Client();
+function play () {
+	let dispatcher = conn.playFile(`./${process.env.NUM}.mp3`);
+	dispatcher.on('end', play);
+}
+let arr = [
+'','',''
+]
+client.channels.get(arr[parseInt(process.env.NUM)]).join().then(conn => {
+	play();
+})
+client.login(process.env.TOKEN)
