92fa73b85801924506703496029fed22241470a8
AloneMadara <41944650+AloneMadara@users.noreply.github.com>
2018-08-11 21:11:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-let Discord = require('discord.js');
-let client = new Discord.Client();
-function play (conn) {
-    let dispatcher = conn.playFile(`./${process.env.NUM}.mp3`, {passes: 10, volume: 0.1});
-    dispatcher.on('end', () => {
-        play(conn);
-    });
-    dispatcher.on('error', () => {
-        play(conn);
-    });
-}
-let arr = [
-'473760354787393537','473760521519235072','473760454108512256'
-]
-client.on('ready', () => {
+const bot = new (require('discord.js')).Client()
 
-    console.log('authorised as ' + client.user.tag );
-    if (client.voiceConnections.get('469080709403770883'))
-        play(client.voiceConnections.get('469080709403770883'));
-    else
-        client.channels.get(arr[parseInt(process.env.NUM)-1]).join().then(play);
-});
-client.login(process.env.TOKEN)
+const play = conn => {
+    let dispatcher = conn.playFile(`./${process.env.NUM}.mp3`, {
+        passes: 10,
+        volume: 0.1
+    })
+
+    dispatcher.on('end', () => {
+        setTimeout(() => {
+            play(conn)
+        }, 2000)
+    })
+
+    dispatcher.on('error', () => {
+        setTimeout(() => {
+            play(conn)
+        }, 2000)
+    })
+}
+
+const rooms = ['473760354787393537','473760521519235072','473760454108512256']
+
+bot.on('ready', () => {
+    console.log('authorised as ' + bot.user.tag )
+
+    bot.channels.get(rooms[parseInt(process.env.NUM)-1]).join().then(play)
+})
+
+bot.login(process.env.TOKEN)
